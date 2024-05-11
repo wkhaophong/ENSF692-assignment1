b7821c59f90b8bf6665a3db6401bd2b214f40859
Warisa Khaophong <warisanut@gmail.com>
2024-05-11 15:15:34 -0600

    Changed name and added best skill

--- assignment1code.py
+++ assignment1code.py
@@ -2,9 +2,10 @@
 # Assignment 1 Starter Code
 # Add your name here!
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
+studentName = "Warisa Khaophong"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
 # print your best skill here
+print("I have a talent for making delicious Asian bread from scratch.")
